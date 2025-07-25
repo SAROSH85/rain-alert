6a2ffcc55fe68181f1d7dfcdc29e096ad8893404
Sarosh <saroshxolo@gmail.com>
2025-07-25 11:20:41 +0000

    Fix: support chart image and token bug in Telegram alert

--- notify.py
+++ notify.py
@@ -5,20 +5,27 @@ from email.mime.text import MIMEText
 from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, EMAIL_SENDER, EMAIL_RECEIVER
 
 def send_telegram_alert(message: str, chart_path: str = None):
-    token = TELEGRAM_TOKEN
+    """
+    Sends a Telegram alert with optional image chart.
+    """
+    token = TELEGRAM_BOT_TOKEN  # fixed typo
     chat_id = TELEGRAM_CHAT_ID
-    send_url = f"https://api.telegram.org/bot{token}/sendMessage"
 
-    # Send text alert
-    requests.post(send_url, data={"chat_id": chat_id, "text": message})
-
-    # If chart is available, send chart image
     if chart_path and os.path.exists(chart_path):
-        files = {'photo': open(chart_path, 'rb')}
-        send_photo_url = f"https://api.telegram.org/bot{token}/sendPhoto"
-        requests.post(send_photo_url, data={"chat_id": chat_id}, files=files)
+        # Send photo with caption
+        with open(chart_path, 'rb') as photo:
+            files = {'photo': photo}
+            data = {'chat_id': chat_id, 'caption': message, 'parse_mode': 'HTML'}
+            requests.post(f"https://api.telegram.org/bot{token}/sendPhoto", data=data, files=files)
+    else:
+        # Send plain text message
+        data = {'chat_id': chat_id, 'text': message, 'parse_mode': 'HTML'}
+        requests.post(f"https://api.telegram.org/bot{token}/sendMessage", data=data)
 
 def send_email_alert(subject, body):
+    """
+    Sends an email using Gmail SMTP.
+    """
     try:
         msg = MIMEText(body)
         msg["Subject"] = subject
