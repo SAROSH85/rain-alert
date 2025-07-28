cd6a396c3b5d84deeeaecdeff31b9e3eb58b5c5e
SAROSH85 <saroshxolo@gmail.com>
2025-07-28 10:24:05 +0000

    Fixed config.py content

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
-<PASTE YOUR UPDATED CONFIG.PY CONTENT HERE>
+import os
+
+WINDY_API_KEY = os.getenv("9S9NyPXTCsB882Lbvjw1EH56EfwHQ9IQ")
+ACCU_API_KEY = os.getenv("JSZBzteqfPETsbKZIfUWGaRdXFRNuWSV")
+LAT = float(os.getenv("LAT", 19.0760))
+LON = float(os.getenv("LON", 72.8777))
+TELEGRAM_BOT_TOKEN = os.getenv("8007450378:AAGXc-2Y1PbyWn-7yDbq_wIG3-WWrQ-DULY")
+TELEGRAM_CHAT_ID = os.getenv("8001098705")
+EMAIL_SENDER = os.getenv("saroshxolo@gmail.com")
+EMAIL_RECEIVER = os.getenv("saroshxolo@gmail.com")
+EMAIL_PASSWORD = os.getenv("uhugfnjanfmijzmh")
+GFS_URL = "https://mausam.imd.gov.in/api/gfs/mumbai.json"
+RADAR_IMAGE_URL = "https://mausam.imd.gov.in/radar/mumbai_latest.png"
